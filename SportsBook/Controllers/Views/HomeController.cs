f3930c4fec00515e4bf5fada37c50f582aa33cbc
prunce <cprunty@greatland.com>
2020-06-04 14:13:34 -0500

    fix build errors

--- SportsBook/Controllers/Views/HomeController.cs
+++ SportsBook/Controllers/Views/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SportsBook.Interfaces;
+using SportsBook.Models.Database;
 using SportsBook.Models;
 using SportsBook.Repository;
 
--- SportsBook/Models/Database/GameTypeEnum.cs
+++ SportsBook/Models/Database/GameTypeEnum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SportsBook.Models
+namespace SportsBook.Models.Database
 {
     public enum GameTypeEnum
     {
