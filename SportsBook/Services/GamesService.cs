c44093af23955929a4ab39d3a61db9de3f2c9d77
Cullen Prunty <cprunty4@yahoo.com>
2021-07-01 03:51:02 -0500

    Switch game slate view to service code

--- SportsBook/Services/GamesService.cs
+++ SportsBook/Services/GamesService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 using SportsBook.Interfaces;
 using SportsBook.Models;
@@ -8,13 +10,38 @@ namespace SportsBook.Services
     {
         private string baseUrl = string.Empty;
         private readonly IConfiguration _configuration;
-        public GamesService(IConfiguration configuration) {
+
+        IGameSlateRepository _gameSlateRepository;
+        public GamesService(IConfiguration configuration,
+        IGameSlateRepository gameSlateRepository) {
             _configuration = configuration;
             baseUrl = _configuration.GetSection("EntitiesApiOptions").GetValue<string>("BaseUrl");             
+            _gameSlateRepository = gameSlateRepository;            
         }
         public GamesSearchResponse GetGamesSearch(GamesSearchRequest request)
         {
-            throw new System.NotImplementedException();
+            GamesSearchResponse gamesSearchResponse = new GamesSearchResponse();
+            List<GameSlate> gameSlates = _gameSlateRepository.AllGameSlates;
+
+            PagingOptionsModel pagingOptions = new PagingOptionsModel();
+            if (request.pagingOptions != null)
+                pagingOptions = request.pagingOptions;
+
+            // Implement paging
+            var pagedGameSlates = gameSlates.Skip(pagingOptions.PageSize * (pagingOptions.Page - 1))
+                                        .Take(pagingOptions.PageSize).ToList();
+
+            // add paging response
+            gamesSearchResponse.pagedResult = new PagedResultModel
+            {
+                TotalResultCount = gameSlates.Count,
+                PagingOptions = pagingOptions,
+                HasMoreResults = gameSlates.Count > pagingOptions.Page * pagingOptions.PageSize
+            };
+
+            gamesSearchResponse.GameSlates = pagedGameSlates;                
+
+            return gamesSearchResponse;
         }
     }
 }
