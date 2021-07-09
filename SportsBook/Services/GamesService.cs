00ef3be21bcff6462fe7acd0a6b6f857e5376fb9
Cullen Prunty <cprunty4@yahoo.com>
2021-07-09 13:28:11 -0500

    fix games filter

--- SportsBook/Services/GamesService.cs
+++ SportsBook/Services/GamesService.cs
@@ -23,67 +23,53 @@ namespace SportsBook.Services
         {
             GamesSearchResponse gamesSearchResponse = new GamesSearchResponse();
             List<GameSlate> gameSlates = _gameSlateRepository.AllGameSlates;
-
-            // Implement filtering here
+            List<GameSlate> filteredGameSlates = new List<GameSlate>();
+            filteredGameSlates = gameSlates;
+            // Implement teamName filtering
             if (!string.IsNullOrEmpty(request.teamName))
             {
-                List<GameSlate> filteredGameSlates = new List<GameSlate>();
-
                 if (gameSlates.Any(x => x.HomeTeamFullName != null
                 && x.HomeTeamFullName.ToUpper().Contains(request.teamName.ToUpper())))
                 {
-                    filteredGameSlates.AddRange(gameSlates.Where(x => x.HomeTeamFullName != null
-                    && x.HomeTeamFullName.ToUpper().Contains(request.teamName.ToUpper())).ToList());
+                    filteredGameSlates = gameSlates.Where(x => x.HomeTeamFullName != null
+                    && x.HomeTeamFullName.ToUpper().Contains(request.teamName.ToUpper())).ToList();
                 }
+                
                 if (gameSlates.Any(x => x.AwayTeamFullName != null
                 && x.AwayTeamFullName.ToUpper().Contains(request.teamName.ToUpper())))
                 {   
-                    //TODO check for dupes
                     filteredGameSlates.AddRange(gameSlates.Where(x => x.AwayTeamFullName != null
                     && x.AwayTeamFullName.ToUpper().Contains(request.teamName.ToUpper())).ToList());
 
                 }
-                // check for Start/End date filtering
-                if (request.startDate != System.DateTime.MinValue)
-                {
-                    if (filteredGameSlates.Count == 0)
-                        filteredGameSlates = gameSlates;
-
-                    filteredGameSlates = filteredGameSlates.Where(x => x.GameStartDateTime != null
-                    && x.GameStartDateTime >= request.startDate).ToList();
-
-                }
-                if (request.endDate != System.DateTime.MinValue)
-                {
-                    if (filteredGameSlates.Count == 0)
-                        filteredGameSlates = gameSlates;
-
-                    filteredGameSlates = filteredGameSlates.Where(x => x.GameStartDateTime != null
-                    && x.GameStartDateTime <= request.endDate).ToList();
-
-                }
-
-
-                if (filteredGameSlates.Count > 0)
-                    gameSlates = filteredGameSlates;
             }
 
-            
+            // check for Start/End date filtering
+            if (request.startDate != System.DateTime.MinValue
+                && request.endDate != System.DateTime.MinValue)
+            {
+                filteredGameSlates = filteredGameSlates.Where(x => 
+                x.GameStartDateTime != null
+                && x.GameStartDateTime >= request.startDate
+                && x.GameStartDateTime <= request.endDate
+                ).ToList();
+
+            }
 
             PagingOptionsModel pagingOptions = new PagingOptionsModel();
             if (request.pagingOptions != null)
                 pagingOptions = request.pagingOptions;
 
             // Implement paging
-            var pagedGameSlates = gameSlates.Skip(pagingOptions.PageSize * (pagingOptions.Page - 1))
+            var pagedGameSlates = filteredGameSlates.Skip(pagingOptions.PageSize * (pagingOptions.Page - 1))
                                         .Take(pagingOptions.PageSize).ToList();
 
             // add paging response
             gamesSearchResponse.pagedResult = new PagedResultModel
             {
-                TotalResultCount = gameSlates.Count,
+                TotalResultCount = filteredGameSlates.Count,
                 PagingOptions = pagingOptions,
-                HasMoreResults = gameSlates.Count > pagingOptions.Page * pagingOptions.PageSize
+                HasMoreResults = filteredGameSlates.Count > pagingOptions.Page * pagingOptions.PageSize
             };
 
             gamesSearchResponse.GameSlates = pagedGameSlates;
