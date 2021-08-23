e4bd1e91d0f0672665aedc483722bb5df113c2b0
CE Prunty <cprunty4@gmail.com>
2021-08-23 09:17:54 -0500

    fix comments page logo image (#45)
    Co-authored-by: Cullen Prunty <cprunty@greatland.com>

--- SportsBook/Controllers/CommentsController.cs
+++ SportsBook/Controllers/CommentsController.cs
@@ -9,21 +9,28 @@ namespace SportsBook.Controllers
     {
         private readonly ILogger<CommentsController> _logger;
         private readonly ICommentsRepository _commentsRepository;
+
+        private readonly IAzureBlobService _azureBlobService;
+
         public CommentsController(ILogger<CommentsController> logger,
-            ICommentsRepository commentsRepository
-        
+            ICommentsRepository commentsRepository,
+            IAzureBlobService azureBlobService,
         )
         {
             _logger = logger;
             _commentsRepository = commentsRepository;
+            _azureBlobService = azureBlobService;
         }
 
         public IActionResult Index(int teamId)
         {
             _logger.LogInformation("entered Comments controller");
             CommentsData commentsData = this._commentsRepository.GetCommentsData(teamId);
+
+            commentsData.LogoImage = _azureBlobService.GetImageUri(commentsData.LogoImage);
+
             return View(commentsData);
-        }        
+        }
 
     }
 }
